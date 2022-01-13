0a3b3624f7b6aa5ccf567e9c9879cd04ad4031e2
julian776 <juliancamiloalvarez77@gmail.com>
2022-01-13 17:15:10 -0500

    Separador incial único terminado y todos los test anteriormente hechos refinados

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,61 +1,66 @@
 package calculator;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.Optional;
 
-import static java.lang.Float.isFinite;
 import static java.lang.Float.parseFloat;
 
 public class StringCalculator {
 
-    public Float resultByList(String lista){
+    public Float resultFromListString(String lista){
         Objects.requireNonNull(lista, "List can not be null");
 
         if(lista.isEmpty()) {
             return 0F;
         }
-        if(lista.length() == 1 && lista.matches("[0-9]")){
+        if(lista.split("").length == 1 && (parseFloat(lista) > 0 && parseFloat(lista) < 1001)){
             return parseFloat(lista);
         }
         //Complex Calculations
-        var delimiter = getDelimiters(lista);
+        var delimiter = getDelimiterAndList(lista);
         String [] splitLista;
         if (delimiter == null){
             splitLista = lista.split("[,\n]");
         }
-        else{
-            splitLista = lista.split(delimiter);
+        else if(delimiter[0].length() == 1){
+            splitLista = delimiter[1].split(delimiter[0]);
         }
-        return Arrays.stream(splitLista).filter(element ->  {
-                    try{
-                        parseFloat(element);
-                    }catch (Exception e){
-                        return false;
-                    }
-                    return true;
-                    })
+        else {
+            splitLista = lista.split("[,\n]");
+        }
+        return Arrays.stream(splitLista)
+                .filter(this::validateElement)
                     .map(element -> parseFloat(element))
                     .reduce((val1, val2) ->  val1 + val2).orElse(0F);
     }
 
-    public String getDelimiters(String lista){
-        String [] splitLista;
-        if (lista.matches("\n")){
-            splitLista = lista.split("\n");
+    public boolean validateElement(String element){
+        try{
+            parseFloat(element);
+        }catch (Exception e){
+            return false;
         }
-        else {
-            splitLista = lista.split(",");
+        if (parseFloat(element) < 0){
+            throw new IllegalArgumentException("Negative number found");
         }
-        for (String value: splitLista){
-            try{
-                parseFloat(value);
-                if(parseFloat(value) < 1001F){
-                    return null;
-                }
-            }catch (Exception e){
-                return value;
+        return true;
+    }
+
+    public String [] getDelimiterAndList(String lista){
+        //Also validates if no delimiter to work with
+        String [] splitLista = lista.split("[,\n]");
+        try{
+            parseFloat(splitLista[0]);
+            return null;
+        }catch (Exception e){
+            if (splitLista.length == 2 && splitLista[0].length() == 1){
+                var delimiter = new String [2];
+                delimiter[0] = splitLista[0];
+                delimiter[1] = splitLista[1];
+                return delimiter;
+            }
+            for (String value : splitLista) {
+
             }
         }
         return null;
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -2,7 +2,7 @@ package calculator;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -13,40 +13,57 @@ class StringCalculatorTest {
 
     @Test
     void emptyList(){
-        assertEquals(0F, calculator.resultByList(""));
+        assertEquals(0F, calculator.resultFromListString(""));
     }
 
     @Test
     void oneNumberOnly(){
-        assertEquals(4F,calculator.resultByList("4"));
-        assertEquals(9F, calculator.resultByList("9"));
+        assertEquals(4F,calculator.resultFromListString("4"));
+        assertEquals(29F, calculator.resultFromListString("29"));
+        assertEquals(950F, calculator.resultFromListString("950"));
     }
 
     @Test
     void twoNumbersByComa(){
-        assertEquals(14F, calculator.resultByList("8,6"));
-        assertEquals(17F, calculator.resultByList("10,7"));
+        assertEquals(14F, calculator.resultFromListString("8,6"));
+        assertEquals(17F, calculator.resultFromListString("10,7"));
     }
 
     @Test
     void twoNumbersByEndLine(){
-        assertEquals(14F, calculator.resultByList("4\n10"));
-        assertEquals(24F, calculator.resultByList("9\n15"));
+        assertEquals(14F, calculator.resultFromListString("4\n10"));
+        assertEquals(24F, calculator.resultFromListString("9\n15"));
     }
 
     @Test
-    void threeNumbers(){
-        assertEquals(21F, calculator.resultByList("4#7@10"));
-        assertEquals(49F, calculator.resultByList("9/7#33"));
+    void testFirstLineDelimiters(){
+        assertEquals(21F, calculator.resultFromListString("#\n7#10#4"));
+        assertEquals(60F, calculator.resultFromListString("-\n7-33-20"));
     }
 
-    @Test()
+    @Test
+    void threeNumbersByAnyDelimiter(){
+        assertEquals(21F, calculator.resultFromListString("#%7#!@10##$3"));
+        assertEquals(49F, calculator.resultFromListString("%7$33$$%205"));
+    }
+
+    @Test
+    void anyDelimiter(){
+        assertEquals(21F, calculator.resultFromListString("*7$$#10/3"));
+        assertEquals(21F, calculator.resultFromListString("#7@10/3"));
+        assertEquals(21F, calculator.resultFromListString("#7@10/3"));
+        assertEquals(21F, calculator.resultFromListString("#7@10/3"));
+    }
+
+    @Test
     void notNegativesAllowed(){
         Exception exception = assertThrows(IllegalArgumentException.class, () ->
-                calculator.resultByList("-3, -7"));
-        assertEquals("", exception.getMessage());
+                calculator.resultFromListString("-3,-7"));
+        assertEquals("Negative number found", exception.getMessage());
     }
 
-
-
+    @Test
+    void any(){
+        Arrays.stream("$\n7$33$20".split("\n$")).forEach(System.out::println);
+    }
 }
